d34a307e6d20638a2f3a6b05b2a1bf11194919a9
dueityou <dueityou@gmail.com>
2025-06-01 18:04:30 +0900

    [add] URLから言語を判断して切り替える処理を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  private
+  
+  def switch_locale(&action)
+    I18n.with_locale(locale, &action)
+  end
+
+  def locale
+    @locale ||= params[:locale] || I18n.default_locale
+  end
 end
