14c322efa4a47ca18fe855b0bf9d1a086aca5af5
dueityou <dueityou@gmail.com>
2025-06-03 00:47:11 +0900

    [fix] lintエラー

--- app/controllers/league_of_legends/top_controller.rb
+++ app/controllers/league_of_legends/top_controller.rb
@@ -1,7 +1,7 @@
 class LeagueOfLegends::TopController < ApplicationController
   http_basic_authenticate_with(
-    name: ENV['BASIC_AUTH_NAME'],
-    password: ENV['BASIC_AUTH_PASSWORD']
+    name: ENV["BASIC_AUTH_NAME"],
+    password: ENV["BASIC_AUTH_PASSWORD"]
     )
 
   def index
