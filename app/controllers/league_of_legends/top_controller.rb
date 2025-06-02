cbae36ad4742d764ced236d6c1624c23c5fdfb35
dueityou <dueityou@gmail.com>
2025-06-03 00:08:51 +0900

    [add] BASIC認証のユーザー名とパスワードを秘匿化

--- app/controllers/league_of_legends/top_controller.rb
+++ app/controllers/league_of_legends/top_controller.rb
@@ -1,5 +1,9 @@
 class LeagueOfLegends::TopController < ApplicationController
-  http_basic_authenticate_with name: "dev", password: "private-dev"
+  http_basic_authenticate_with(
+    name: ENV['BASIC_AUTH_NAME'],
+    password: ENV['BASIC_AUTH_PASSWORD']
+    )
+
   def index
   end
 end
