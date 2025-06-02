cad8fd09508697e0518157b4161852f584f4130f
dueityou <dueityou@gmail.com>
2025-06-02 15:17:40 +0900

    [add] 日本語圏からroot_pathにアクセスがあった場合に、/ja/lolにリダイレクトされることを確認するテストを追加

--- spec/system/top_page_spec.rb
+++ spec/system/top_page_spec.rb
@@ -5,7 +5,15 @@ RSpec.describe 'トップページ', type: :feature do
     it '"/en/lol"にリダイレクトされる' do
       page.driver.header 'Accept-Language', 'en'
       visit root_path
-      expect(current_path).to eq("/en/lol")
+      expect(current_path).to eq('/en/lol')
+    end
+  end
+
+  context '日本語圏から"/"にアクセスがあった場合' do
+    it '"/ja/lol"にリダイレクトされる' do
+      page.driver.header 'Accept-Language', 'ja'
+      visit root_path
+      expect(current_path).to eq('/ja/lol')
     end
   end
 end
