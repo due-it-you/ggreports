79d69f5a5c9768339c7b3fb99328fff3b99f288d
dueityou <dueityou@gmail.com>
2025-06-03 11:51:38 +0900

    [add] searched_players/matches#index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.map(&:to_s).join('|')}/ do
     namespace :league_of_legends, path: "lol" do
       root to: "top#index"
-      resources :searched_players, only: %i[ create ]
+      resources :searched_players, only: %i[ create ] do
+        resources :matches, only: %i[ index ]
+      end
     end
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
