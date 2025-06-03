9eba71dfcf1c4db4dd04f4729e2fe9b7c9616f1a
dueityou <dueityou@gmail.com>
2025-06-03 14:38:05 +0900

    [add] サモナー検索のストロングパラメータの新規作成

--- app/controllers/league_of_legends/summoners_controller.rb
+++ app/controllers/league_of_legends/summoners_controller.rb
@@ -1,4 +1,10 @@
 class LeagueOfLegends::SummonersController < ApplicationController
   def create
   end
+
+  private
+
+  def summoners_params
+    params.require(:summoner).permit(:name)
+  end
 end
