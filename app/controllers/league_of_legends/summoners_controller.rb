57e2ac02e19e31330440fe7b3c92d33a559d1cf7
dueityou <dueityou@gmail.com>
2025-06-03 15:08:11 +0900

    [add] 入力されたサモナーのpuuidの取得する処理の追加

--- app/controllers/league_of_legends/summoners_controller.rb
+++ app/controllers/league_of_legends/summoners_controller.rb
@@ -1,6 +1,18 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
 class LeagueOfLegends::SummonersController < ApplicationController
   def create
     name, tagline = summoners_params[:name].gsub(" ","").split('#', 2)
+  
+    # 入力されたサモナーのpuuidの取得
+    platform_routing_value = "na1.api.riotgames.com"
+    regional_routing_value = "americas.api.riotgames.com"
+    api = "/riot/account/v1/accounts/by-riot-id/#{name}/#{tagline}?api_key=#{ENV['RIOT_DEVELOPMENT_API']}"
+    uri = URI.parse("https://#{regional_routing_value}#{api}")
+    response_data = Net::HTTP.get(uri)
+    summoner_data = JSON.parse(response_data)
   end
 
   private
