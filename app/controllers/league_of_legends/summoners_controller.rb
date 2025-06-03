8c7abc44a862c0d9cfd62bfc70936c8140eab330
dueityou <dueityou@gmail.com>
2025-06-03 16:27:22 +0900

    [fix] Riot APIのリクエストのX-Riot-TokenヘッダーにAPIを渡してリクエストを飛ばさないことでAPI取得の不具合が発生していたので修正

--- app/controllers/league_of_legends/summoners_controller.rb
+++ app/controllers/league_of_legends/summoners_controller.rb
@@ -10,11 +10,16 @@ class LeagueOfLegends::SummonersController < ApplicationController
     platform_routing_value = "na1.api.riotgames.com"
     platform = platform_routing_value.split('.').first
     regional_routing_value = "americas.api.riotgames.com"
-    api = "/riot/account/v1/accounts/by-riot-id/#{name}/#{tagline}?api_key=#{ENV['RIOT_DEVELOPMENT_API']}"
+    api = "/riot/account/v1/accounts/by-riot-id/#{name}/#{tagline}"
     uri = URI.parse("https://#{regional_routing_value}#{api}")
-    response_data = Net::HTTP.get(uri)
-    summoner_data = JSON.parse(response_data)
-    
+    request = Net::HTTP::Get.new(uri)
+    request["X-Riot-Token"] = ENV['RIOT_DEVELOPMENT_API']
+    response_data = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+      http.request(request)
+    end
+    summoner_data = JSON.parse(response_data.body)
+    binding.pry
+
     summoner = Summoner.new({
                     name: summoner_data["gameName"],
                     tagline: summoner_data["tagLine"],
