7ec95456ea2378053eaad8e9538dc1fbe4964327
dueityou <dueityou@gmail.com>
2025-06-03 18:31:42 +0900

    [add] 直近5試合の試合の詳細なデータを取得する処理の追加

--- app/controllers/league_of_legends/summoners/matches_controller.rb
+++ app/controllers/league_of_legends/summoners/matches_controller.rb
@@ -7,7 +7,7 @@ class LeagueOfLegends::Summoners::MatchesController < ApplicationController
     searched_summoner = Summoner.find(params[:summoner_id])
     puuid = searched_summoner.puuid
 
-    # 入力されたサモナーのpuuidの取得
+    # 入力されたサモナーの試合のリストを取得
     platform_routing_value = "na1.api.riotgames.com"
     platform = platform_routing_value.split(".").first
     regional_routing_value = "americas.api.riotgames.com"
@@ -19,6 +19,19 @@ class LeagueOfLegends::Summoners::MatchesController < ApplicationController
       http.request(request)
     end
     matches_data = JSON.parse(response_data.body)
-    matches_data_limit_5 = summoner_data.slice(0,5)
+    matches_data_limit_5 = matches_data.slice(0,5)
+
+    matches_data_arr = []
+
+    matches_data_limit_5.each do |match_id|
+      api = "/lol/match/v5/matches/#{match_id}"
+      uri = URI.parse("https://#{regional_routing_value}#{api}")
+      request = Net::HTTP::Get.new(uri)
+      request["X-Riot-Token"] = ENV["RIOT_DEVELOPMENT_API"]
+      response_data = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+        http.request(request)
+      end
+      @matches_data_arr << JSON.parse(response_data.body)
+    end
   end
 end
