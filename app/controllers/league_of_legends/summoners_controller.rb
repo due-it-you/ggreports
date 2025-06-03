22176851fef81d9f56a43db70741eaddc5eea08d
dueityou <dueityou@gmail.com>
2025-06-03 17:35:25 +0900

    [fix] lintエラー

--- app/controllers/league_of_legends/summoners_controller.rb
+++ app/controllers/league_of_legends/summoners_controller.rb
@@ -1,19 +1,19 @@
-require 'net/http'
-require 'uri'
-require 'json'
+require "net/http"
+require "uri"
+require "json"
 
 class LeagueOfLegends::SummonersController < ApplicationController
   def create
-    name, tagline = summoners_params[:name].gsub(" ","").split('#', 2)
-  
+    name, tagline = summoners_params[:name].gsub(" ", "").split("#", 2)
+
     # 入力されたサモナーのpuuidの取得
     platform_routing_value = "na1.api.riotgames.com"
-    platform = platform_routing_value.split('.').first
+    platform = platform_routing_value.split(".").first
     regional_routing_value = "americas.api.riotgames.com"
     api = "/riot/account/v1/accounts/by-riot-id/#{name}/#{tagline}"
     uri = URI.parse("https://#{regional_routing_value}#{api}")
     request = Net::HTTP::Get.new(uri)
-    request["X-Riot-Token"] = ENV['RIOT_DEVELOPMENT_API']
+    request["X-Riot-Token"] = ENV["RIOT_DEVELOPMENT_API"]
     response_data = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
       http.request(request)
     end
